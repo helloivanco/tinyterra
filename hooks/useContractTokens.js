556afc1b4c46a67fe42f7501009a6c0377cdbf49
Paid_co <hellopaidco@gmail.com>
2021-09-03 16:47:42 +0100

    tokenomics

---
+++ hooks/useContractTokens.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import useLCDClient from './useLCDClient';
+
+export default function useContractTokens() {
+  const lcdClient = useLCDClient();
+  const [contractTokens, setContractTokens] = useState([]);
+
+  useEffect(async () => {
+    if (lcdClient) {
+      const x = await lcdClient.wasm.contractQuery(
+        lcdClient.mainContractAddress,
+        { all_creator_tokens: {} }
+      );
+      console.log(x.tokens);
+      setContractTokens(x.tokens);
+    }
+  }, [lcdClient]);
+
+  return contractTokens;
+}
